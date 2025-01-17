c0d1f757f30801e4d7b6bb50e9bf98e05ab42ed4
Manoj Vanga <60837576+MANOJSAHIT@users.noreply.github.com>
2025-01-16 16:45:14 -0800

    Update app.py

--- BIM-CAD/app.py
+++ BIM-CAD/app.py
@@ -1,27 +1,16 @@
-# app.py
-
 import base64
 from io import BytesIO
 import google.generativeai as genai
 import gradio as gr
 from PIL import Image
-
-# Set your Gemini API Key
 genai.configure(api_key="AIzaSyBw7z4Pvp0iSY5vz0tP8B9mnKv9HZSnYzQ") 
 
 def extract_floorplan_data(image):
-    """Extracts floor plan data required for 3D model generation 
-    using Google Gemini."""
     try:
-        # Convert the image to bytes
         buffered = BytesIO()
         image.save(buffered, format="JPEG") 
         image_bytes = buffered.getvalue()
-
-        # Prepare the image data for the API call
         image_data = base64.b64encode(image_bytes).decode("utf-8")
-
-        # Call the model with the prompt and image data
         with open('prompt.txt','r') as file:
             prompt=file.read()
         model = genai.GenerativeModel(model_name="gemini-1.5-pro")
@@ -36,10 +25,7 @@ def extract_floorplan_data(image):
         return "Error processing image. See console for details."
 
 def process_image(image):
-    """Processes the image and returns the extracted floor plan data."""
     floorplan_data = extract_floorplan_data(image)
-
-    # Pass the JSON data to the JavaScript function
     return floorplan_data, gr.HTML(
         f"""
         <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"></script>
@@ -49,8 +35,6 @@ def process_image(image):
         </script>
         """
     )
-
-# Define Gradio interface
 iface = gr.Interface(
     fn=process_image,
     inputs=gr.Image(type="pil"),
